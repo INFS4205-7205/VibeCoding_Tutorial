8b2979f288423d301332fa91ac4623ec144336bf
Danny Wang <danny.wang@uq.edu.au>
2026-02-20 14:55:57 +1000

    fixed footer format

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <ModuleGrid />
       </main>
       <footer className="py-6 md:px-8 md:py-0 w-full">
-        <div className="container flex flex-col items-center justify-center gap-2 md:h-24 px-4 text-center">
+        <div className="container mx-auto flex flex-col items-center justify-center gap-2 md:h-24 px-4 text-center">
           <p className="text-sm leading-loose text-muted-foreground break-words w-full max-w-screen-lg">
             Team: <span className="font-bold text-foreground">Danny Wang</span><sup>*</sup>, Yadan Luo<sup>†</sup>, Zhuoxiao Chen, Yan Jiang, Xiangyu Sun, Xuwei Xu, Fengyi Zhang, Zhizhen Zhang.
           </p>
